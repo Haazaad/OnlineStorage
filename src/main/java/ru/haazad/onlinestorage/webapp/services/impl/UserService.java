ed41722690219957b667ca2507519314739e7ba8
Artem <smirnov.artem.sam@gmail.com>
2021-09-09 20:13:29 +0400

    небольшие правки

--- src/main/java/ru/haazad/onlinestorage/webapp/services/impl/UserService.java
+++ src/main/java/ru/haazad/onlinestorage/webapp/services/impl/UserService.java
@@ -6,19 +6,27 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.haazad.onlinestorage.webapp.dtos.UserDto;
 import ru.haazad.onlinestorage.webapp.models.Role;
 import ru.haazad.onlinestorage.webapp.models.User;
 import ru.haazad.onlinestorage.webapp.repositories.UserRepository;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
+    private static final String DEFAULT_USER_ROLE = "ROLE_USER";
+
     private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final BCryptPasswordEncoder encoder;
 
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -34,7 +42,17 @@ public class UserService implements UserDetailsService {
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 
-    public void addNewUser(User user) {
+    @Transactional
+    public void addNewUser(UserDto userDto) {
+        User user = new User();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(encoder.encode(userDto.getPassword()));
+        user.setEmail(userDto.getEmail());
+        if (userDto.getRole() == null) {
+            user.setRoles(Collections.singletonList(roleService.findByRoleName(DEFAULT_USER_ROLE)));
+        } else {
+            user.setRoles(Collections.singletonList(roleService.findByRoleName(userDto.getRole())));
+        }
         userRepository.save(user);
     }
 }
